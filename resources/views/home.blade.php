16ec8b62e94f8217827df1893692de9461ca374e
Fauzi Rachman <fauziiirachman@gmail.com>
2018-02-13 06:26:50 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.app')
+
+@section('content')
+@if (session('status'))
+<div class="card card-default">
+
+    <div class="card-body">
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>        
+    </div>
+
+</div>
+<br>
+@endif
+
+<div class="card card-default">
+
+    <div class="card-body">
+        <input type="text" class="form-control" name="search" placeholder="find your place...">
+    </div>
+
+    <div class="jumbotron jumbotron-fluid" style="margin-bottom: 0px;">
+      <div class="container">
+        <h1 class="display-4">Discover Your Best Holiday with HoleyGo</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div>
+    </div>
+
+</div>
+
+<br>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Free Member</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link" data-toggle="modal" data-target="#FreeMemberModal">See What It's Value</a>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Premium User</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link" data-toggle="modal" data-target="#PremiumUserModal">See What It's Value</a>
+          </div>
+          
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="FreeMemberModal" tabindex="-1" role="dialog" aria-labelledby="FreeMemberModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Free Member Advantages</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="PremiumUserModal" tabindex="-1" role="dialog" aria-labelledby="PremiumUserModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Premium User Advantages</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+      </div>
+    </div>
+  </div>
+</div>
+
+<br>
+
+<div class="card card-default">
+    
+    <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+        </blockquote>
+    </div>
+
+    <hr>
+
+    <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+        </blockquote>
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        <li><a class="nav-link" href="#">Popular</a></li>
+                        <li><a class="nav-link" href="#">Group</a></li>
+                        <li><a class="nav-link" href="#">Store</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <!--Footer-->
+        <footer class="page-footer center-on-small-only blue-grey lighten-5 pt-0">
+
+            <!--Footer Links-->
+            <div class="container mt-5 mb-4 text-center text-md-left">
+                <div class="row mt-3">
+
+                    <!--First column-->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mb-r dark-grey-text">
+                        <h6 class="title font-bold"><strong>Company name</strong></h6>
+                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+                            amet, consectetur adipisicing elit.</p>
+                    </div>
+                    <!--/.First column-->
+
+                    <!--Second column-->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-r dark-grey-text">
+                        <h6 class="title font-bold"><strong>Products</strong></h6>
+                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p><a href="#!" class="dark-grey-text">MDBootstrap</a></p>
+                        <p><a href="#!" class="dark-grey-text">MDWordPress</a></p>
+                        <p><a href="#!" class="dark-grey-text">BrandFlow</a></p>
+                        <p><a href="#!" class="dark-grey-text">Bootstrap Angular</a></p>
+                    </div>
+                    <!--/.Second column-->
+
+                    <!--Third column-->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-r dark-grey-text">
+                        <h6 class="title font-bold"><strong>Useful links</strong></h6>
+                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p><a href="#!" class="dark-grey-text">Your Account</a></p>
+                        <p><a href="#!" class="dark-grey-text">Become an Affiliate</a></p>
+                        <p><a href="#!" class="dark-grey-text">Shipping Rates</a></p>
+                        <p><a href="#!" class="dark-grey-text">Help</a></p>
+                    </div>
+                    <!--/.Third column-->
+
+                    <!--Fourth column-->
+                    <div class="col-md-4 col-lg-3 col-xl-3 dark-grey-text">
+                        <h6 class="title font-bold"><strong>Contact</strong></h6>
+                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                        <p><i class="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+                        <p><i class="fa fa-envelope mr-3"></i> info@example.com</p>
+                        <p><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                        <p><i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+                    </div>
+                    <!--/.Fourth column-->
+
+                </div>
+            </div>
+            <!--/.Footer Links-->
+
+            <!-- Copyright-->
+            <div class="footer-copyright">
+                <div class="container-fluid">
+                    © 2017 Copyright: <a href="https://www.MDBootstrap.com"><strong> MDBootstrap.com</strong></a>
+                </div>
+            </div>
+            <!--/.Copyright -->
+            
+        </footer>
+        <!--/.Footer-->
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
