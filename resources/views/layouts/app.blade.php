d8a1765133d26f9fabcf33f6adb9f09dd14f2992
Fauzi Rachman <fauziiirachman@gmail.com>
2018-02-14 05:33:16 +0700

    add popular page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('home') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +27,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li><a class="nav-link" href="#">Popular</a></li>
+                        <li><a class="nav-link" href="{{ route('popular') }}">Popular</a></li>
                         <li><a class="nav-link" href="#">Group</a></li>
                         <li><a class="nav-link" href="#">Store</a></li>
                     </ul>
---
+++ resources/views/popular.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card card-default">
+    <div class="card-header">
+        Popular Travel Plan
+    </div>
+
+    <div class="card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras efficitur dapibus sodales. Aliquam bibendum pulvinar massa, sit amet ullamcorper sem pellentesque ut. Vestibulum vehicula sapien sed cursus blandit. Nam porta tempus lorem vitae pellentesque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed varius enim non.</p>
+    </div>
+
+</div>
+
+<br>
+
+<div class="row">
+    <div class="col-md-4">
+         <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Plan by User A</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Read More</a>
+          </div>
+          
+        </div>
+    </div>
+
+    <div class="col-md-4">
+         <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Plan by User A</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Read More</a>
+          </div>
+          
+        </div>
+    </div>
+
+    <div class="col-md-4">
+         <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Plan by User A</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Read More</a>
+          </div>
+          
+        </div>
+    </div>
+</div>
+
+<br>
+
+<div class="row">
+    <div class="col-md-4">
+         <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Plan by User A</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Read More</a>
+          </div>
+          
+        </div>
+    </div>
+
+    <div class="col-md-4">
+         <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Plan by User A</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Read More</a>
+          </div>
+          
+        </div>
+    </div>
+
+    <div class="col-md-4">
+         <div class="card">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <h5 class="card-title">Plan by User A</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Read More</a>
+          </div>
+          
+        </div>
+    </div>
+</div>
+
+<br>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://i.ytimg.com/vi/fdnDGe58lW0/maxresdefault.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://i.ytimg.com/vi/fdnDGe58lW0/maxresdefault.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://i.ytimg.com/vi/fdnDGe58lW0/maxresdefault.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+@endsection
